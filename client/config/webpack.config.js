999776d62ea5421e37d6395e0d8d25847663605d
christianjosephvarriale <cjvarria@uwaterloo.ca>
2020-02-14 17:23:21 +0100

    depreciated plugin

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -502,21 +502,6 @@ module.exports = function(webpackEnv) {
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': '"production"'
       }),
-      new webpack.optimize.UglifyJsPlugin({
-        mangle: true,
-        compress: {
-          warnings: false, // Suppress uglification warnings
-          pure_getters: true,
-          unsafe: true,
-          unsafe_comps: true,
-          screw_ie8: true
-        },
-        output: {
-          comments: false,
-        },
-        exclude: [/\.min\.js$/gi] // skip pre-minified libs
-      }),
-      new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
       new CompressionPlugin({
         asset: "[path].gz[query]",
         algorithm: "gzip",
