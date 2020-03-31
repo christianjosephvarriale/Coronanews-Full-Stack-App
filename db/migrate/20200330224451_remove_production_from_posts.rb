c6b1d136f64aca3a7d7a05f337e98b91ed26efc3
Christian Varriale <varriale737@gmail.com>
2020-03-31 01:52:28 -0400

    fix migration

--- db/migrate/20200330224451_remove_production_from_posts.rb
+++
@@ -1,4 +0,0 @@
-class RemoveProductionFromPosts < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/migrate/20200331041424_add_prod_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddProdToPosts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :posts, :production, :boolean
-  end
-end
